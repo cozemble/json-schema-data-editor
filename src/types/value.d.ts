bb1935b2e4eb5e5c8c5675201daf4b81a1b8400a
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-03-27 13:38:35 +0300

    Declare value types

---
+++ src/types/value.d.ts
@@ -0,0 +1,9 @@
+declare type SimpleValue = string | number | boolean | null | undefined
+
+declare type ArrayValue = AnyValue[]
+
+declare interface ObjectValue {
+	[key: string]: SimpleValue | ObjectValue | ArrayValue
+}
+
+declare type AnyValue = SimpleValue | ObjectValue | ArrayValue
