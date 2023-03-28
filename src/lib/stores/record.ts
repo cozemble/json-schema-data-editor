41c3b97fe4736058e8f49be8632a3b09d1dc7bc7
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-03-28 15:49:20 +0300

    Record store history system basis and tests

--- src/lib/stores/record.ts
+++ src/lib/stores/record.ts
@@ -3,12 +3,73 @@ import type { Writable } from 'svelte/store'
 import { writable, get } from 'svelte/store'
 import _ from 'lodash'
 
+const HISTORY_THRESHOLD = 1000
+
 export const record: Writable<ObjectValue> = writable({})
 
 export const editValue = (path: Path | SimplePath, value: any) => {
 	const recordValue = _.cloneDeep(get(record))
 
+	const previous = _.get(recordValue, path)
+
+	if (_.isEqual(previous, value)) return
+
 	_.set(recordValue, path, value)
 
+	handleHistory(typeof path === 'string' ? path : path.join(), previous, value)
+
 	record.set(recordValue)
+}
+
+// * HISTORY
+
+export const history: Writable<HistoryEntry[]> = writable([])
+export const historyIndex: Writable<number> = writable(0)
+
+const addToHistory = (path: SimplePath, previous: AnyValue, current: AnyValue) => {
+	const historyValue = _.cloneDeep(get(history))
+	const historyIndexValue = get(historyIndex)
+
+	// if the history index is not at the end, remove the rest of the history because it is no longer valid
+	if (historyIndexValue > 0) history.set(_.cloneDeep(historyValue.slice(0, historyIndexValue)))
+
+	history.set([
+		...historyValue,
+		_.cloneDeep({
+			path,
+			timestamp: Date.now(),
+			previous,
+			current
+		})
+	])
+
+	historyIndex.set(historyValue.length)
+}
+
+const handleHistory = (path: SimplePath, previous: AnyValue, current: AnyValue): void => {
+	if (previous === current) return
+
+	const historyValue = _.cloneDeep(get(history))
+	const lastHistoryEntry = _.cloneDeep(_.last(historyValue))
+
+	// if there is no last history entry, add a new one
+	if (!lastHistoryEntry) return addToHistory(path, previous, current)
+
+	// if the last history entry is not for the same path, add a new one
+	if (lastHistoryEntry.path !== path) return addToHistory(path, previous, current)
+
+	// if the last history entry is for the same path, check if it was less than a second ago
+	const now = Date.now()
+	const timeDifference = now - lastHistoryEntry.timestamp
+
+	if (timeDifference < HISTORY_THRESHOLD) {
+		// update last history entry
+		lastHistoryEntry.current = current
+
+		history.set(_.cloneDeep([...historyValue.slice(0, -1), lastHistoryEntry]))
+
+		return
+	}
+
+	addToHistory(path, previous, current)
 }
--- src/tests/stores/record.test.ts
+++ src/tests/stores/record.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/svelte'
 import { get } from 'svelte/store'
 
-import { record, editValue } from '../../lib/stores/record'
+import { record, editValue, history, historyIndex } from '../../lib/stores/record'
 
 const mockRecord = {
 	name: 'John Doe',
@@ -12,6 +12,12 @@ const mockRecord = {
 		mother: 'Jane Doe',
 		children: ['John Doe Jr.', 'Jane Doe Jr.']
 	}
+}
+
+const reset = () => {
+	history.set([])
+	historyIndex.set(0)
+	record.set(mockRecord)
 }
 
 describe('RECORD STORE', () => {
@@ -58,6 +64,89 @@ describe('RECORD STORE', () => {
 				children: ['Brad Doe Jr.', 'Jane Doe Jr.']
 			})
 			expect(get(record)?.family?.father).toBe('Brad Doe Sr.')
+		})
+	})
+
+	describe('# SHOULD KEEP HISTORY', () => {
+		it('should initialize a history store and index store correctly', () => {
+			reset()
+
+			expect(history).toBeTruthy()
+			expect(historyIndex).toBeTruthy()
+			expect(get(history).length).to.equal(0, 'should have no history entries')
+			expect(get(historyIndex)).to.equal(0, 'should have an index of 0')
+		})
+
+		it('should update the last entry in the history store and not create new entry when a value is edited in less than a second ago', async () => {
+			reset()
+
+			editValue('name', 'Mike Doe')
+
+			await new Promise<void>((resolve) =>
+				setTimeout(() => {
+					editValue('name', 'James Doe')
+					resolve()
+				}, 500)
+			)
+			expect(get(history).length).to.equal(1, 'should have one history entry')
+			expect(get(history)[0].current).to.equal('James Doe', 'should have the last value')
+		})
+
+		it('should create a new history entry on value edit if the previous edit for the value was over 1 second ago', async () => {
+			reset()
+
+			record.set(mockRecord)
+			editValue('name', 'Mike Doe')
+
+			await new Promise<void>((resolve) =>
+				setTimeout(() => {
+					editValue('name', 'James Doe')
+					resolve()
+				}, 1100)
+			)
+
+			expect(get(history).length).to.equal(2, 'should have two history entries')
+			expect(get(history)[0].current).to.equal('Mike Doe', 'should have the first value')
+			expect(get(history)[1].current).to.equal('James Doe', 'should have the last value')
+		})
+
+		it("should create new history with 1 second intervals even if no value edit gap hasn't exceeded 1 second", async () => {
+			reset()
+
+			const mockValues = ['Mike Doe', 'James Doe', 'Jack Doe']
+
+			editValue('name', mockValues[0])
+
+			setTimeout(() => {
+				editValue('name', mockValues[1])
+			}, 500)
+
+			await new Promise<void>((resolve) =>
+				setTimeout(() => {
+					editValue('name', mockValues[2])
+					resolve()
+				}, 1100)
+			)
+
+			const log = get(history)
+			expect(log.length).to.equal(2, 'should have two history entries')
+			expect(log[0].current).to.equal(mockValues[1], '1st log should have the 1st value')
+			expect(log[1].current).to.equal(mockValues[2], '2nd log should have the last value')
+		})
+
+		it('should create a new history entry on edit of a different value then the previous edit', () => {
+			reset()
+
+			record.set(mockRecord)
+			editValue('name', 'Mike Doe')
+			editValue('age', 43)
+
+			const logs = get(history)
+			expect(logs.length).to.equal(2, 'should have two history entries')
+			expect(logs[0].path).to.equal('name', 'should have the first edit path')
+			expect(logs[1].path).to.equal('age', 'should have the last edit path')
+			expect(logs[0].current).to.equal('Mike Doe', 'should have the first value')
+			expect(logs[1].current).to.equal(43, 'should have the last value')
 		})
 	})
 })
