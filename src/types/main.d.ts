d0c2481d8d63dccdac48ad70c6a47be74dd3fa4d
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-03-28 15:48:39 +0300

    Declare HistoryEntry type

--- src/types/main.d.ts
+++ src/types/main.d.ts
@@ -1,3 +1,16 @@
 /** The path to the field in the data */
 declare type Path = string[]
 declare type SimplePath = string
+
+/** History log entry */
+declare interface HistoryEntry {
+	/** The path to the field in the data */
+	path: SimplePath
+	/** To track the time of the change */
+	timestamp: number
+
+	/** The value of the field before the change */
+	previous: AnyValue
+	/** The value of the field after the change */
+	current: AnyValue
+}
